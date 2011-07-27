27911c722b8400c127a7559a86a1b7a934b96902
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-07-27 13:17:07 +0200

    removed print (not wsgi compliant

--- emailvision/utils.py
+++ emailvision/utils.py
@@ -2,30 +2,22 @@ from collections import namedtuple
 from urllib import quote
 
 try:
-  from lxml import etree
-  print("running with lxml.etree")
+    from lxml import etree
 except ImportError:
-  try:
-    # Python 2.5
-    import xml.etree.cElementTree as etree
-    print("running with cElementTree on Python 2.5+")
-  except ImportError:
     try:
-      # Python 2.5
-      import xml.etree.ElementTree as etree
-      print("running with ElementTree on Python 2.5+")
+        # Python 2.5
+        import xml.etree.cElementTree as etree
     except ImportError:
-      try:
-        # normal cElementTree install
-        import cElementTree as etree
-        print("running with cElementTree")
-      except ImportError:
         try:
-          # normal ElementTree install
-          import elementtree.ElementTree as etree
-          print("running with ElementTree")
+            # Python 2.5
+            import xml.etree.ElementTree as etree
         except ImportError:
-          print("Failed to import ElementTree from any known place")
+            try:
+                # normal cElementTree install
+                import cElementTree as etree
+            except ImportError:
+                # normal ElementTree install
+                import elementtree.ElementTree as etree
 
 from httplib2 import Http
 
