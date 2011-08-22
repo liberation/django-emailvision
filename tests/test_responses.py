7cdb39a0c5531fc8d8c01177abcabb3b85827db1
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-08-22 18:28:58 +0200

    added an attribute to configure which class is used to process reponses

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -5,22 +5,23 @@ from emailvision.utils import BaseResponse, GetResponse, PostResponse
 
 
 class BaseResponseTest(TestCase):
+    klass = BaseResponse
 
     def test_find(self):
-        base_response = BaseResponse(u'<xml><a><b></b></a></xml>')
+        base_response = klass(u'<xml><a><b></b></a></xml>')
         a_element = base_response.find('a')
         self.assertIsNotNone(a_element)
         self.assertEquals(len(a_element.getchildren()), 1)
 
     def test_findall(self):
-        base_response = BaseResponse(u'<xml><a><b></b></a><a></a></xml>')
+        base_response = klass(u'<xml><a><b></b></a><a></a></xml>')
         a_elements = base_response.findall('a')
         self.assertEquals(len(a_elements), 2)
         self.assertEquals(len(a_elements[0].getchildren()), 1)
         self.assertEquals(len(a_elements[1].getchildren()), 0)
 
     def test_iterfind(self):
-        base_response = BaseResponse(u'<xml><a><b></b></a><a></a></xml>')
+        base_response = klass(u'<xml><a><b></b></a><a></a></xml>')
         a_elements = base_response.iterfind('a')
         self.assertEquals(type(a_elements), GeneratorType)
         nb_childs_of_a_elements = [1, 0]
